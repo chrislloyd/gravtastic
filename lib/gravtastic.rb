862b7170338bacbba40061c5765599e954706b76
Victor Costan <costan@gmail.com>
2011-03-15 03:08:59 -0400

    Extracted Gravtastic.gravatar_url.

--- lib/gravtastic.rb
+++ lib/gravtastic.rb
@@ -52,32 +52,37 @@ module Gravtastic
 
   module ClassMethods
     attr_accessor :gravatar_source, :gravatar_defaults
-    
-    # Gravtastic abbreviates certain params so that it produces the smallest
-    # possible URL. Every byte counts.
-    def gravatar_abbreviations
-      { :size => 's',
-        :default => 'd',
-        :rating => 'r'
-      }
-    end
   end
 
   module InstanceMethods
+    # Constructs the full Gravatar url.
+    def gravatar_url(options={})
+      email = send(self.class.gravatar_source)
+      options = self.class.gravatar_defaults.merge(options)
+      Gravtastic.gravatar_url email, options
+    end
+
+    # The raw MD5 hash of the users' email.
+    def gravatar_id
+      email = send(self.class.gravatar_source)
+      Gravtastic.gravatar_id email
+    end
+  end
+
+  class <<self
+    # Constructs the full Gravatar url.
+    def gravatar_url(email, options)
+      options = options.merge({})
+      gravatar_hostname(options.delete(:secure)) +
+        gravatar_filename(email, options.delete(:filetype)) +
+        url_params_from_hash(options)
+    end
     
     # The raw MD5 hash of the users' email. Gravatar is particularly tricky as
     # it downcases all emails. This is really the guts of the module, 
     # everything else is just convenience.
-    def gravatar_id
-      Digest::MD5.hexdigest(send(self.class.gravatar_source).to_s.downcase)
-    end
-    
-    # Constructs the full Gravatar url.
-    def gravatar_url(options={})
-      options = self.class.gravatar_defaults.merge(options)
-      gravatar_hostname(options.delete(:secure)) +
-        gravatar_filename(options.delete(:filetype)) +
-        url_params_from_hash(options)
+    def gravatar_id(email)
+      Digest::MD5.hexdigest(email.to_s.downcase)
     end
 
   private
@@ -87,7 +92,7 @@ module Gravtastic
     # therefore be tested easily).
     def url_params_from_hash(hash)
       '?' + hash.map do |key, val|
-        [self.class.gravatar_abbreviations[key.to_sym] || key.to_s, val.to_s ].join('=')
+        [gravatar_abbreviations[key.to_sym] || key.to_s, val.to_s ].join('=')
       end.sort.join('&')
     end
 
@@ -97,9 +102,17 @@ module Gravtastic
     end
     
     # Munges the ID and the filetype into one. Like "abc123.png"
-    def gravatar_filename(filetype)
-      "#{gravatar_id}.#{filetype}"
+    def gravatar_filename(email, filetype)
+      "#{gravatar_id(email)}.#{filetype}"
+    end
+
+    # Gravtastic abbreviates certain params so that it produces the smallest
+    # possible URL. Every byte counts.
+    def gravatar_abbreviations
+      { :size => 's',
+        :default => 'd',
+        :rating => 'r'
+      }
     end
   end
-
 end
