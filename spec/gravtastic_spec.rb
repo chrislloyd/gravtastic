4e07f7a3a7a4357f2349389fd9bd13c7a9e04015
Victor Costan <costan@gmail.com>
2011-09-07 22:33:41 -0400

    Updated specs to reflect HTTPS default.

--- spec/gravtastic_spec.rb
+++ spec/gravtastic_spec.rb
@@ -17,8 +17,8 @@ describe Gravtastic do
 
   describe 'default' do
 
-    it "options are {:rating => 'PG', :secure => false, :filetype => :png}" do
-      @g.gravatar_defaults.should == {:rating => 'PG', :secure => false, :filetype => :png}
+    it "options are {:rating => 'PG', :secure => true, :filetype => :png}" do
+      @g.gravatar_defaults.should == {:rating => 'PG', :secure => true, :filetype => :png}
     end
 
     it "source is :email" do
@@ -47,23 +47,23 @@ describe Gravtastic do
     end
 
     it "makes a pretty URL" do
-      @user.gravatar_url.should == 'http://gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af.png?r=PG'
+      @user.gravatar_url.should == 'https://secure.gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af.png?r=PG'
     end
 
-    it "makes a secure URL" do
-      @user.gravatar_url(:secure => true).should == 'https://secure.gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af.png?r=PG'
+    it "makes an unsecure URL" do
+      @user.gravatar_url(:secure => false).should == 'http://gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af.png?r=PG'
     end
 
     it "makes a jpeggy URL" do
-      @user.gravatar_url(:filetype => :jpg).should == 'http://gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af.jpg?r=PG'
+      @user.gravatar_url(:filetype => :jpg).should == 'https://secure.gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af.jpg?r=PG'
     end
 
     it "makes a saucy URL" do
-      @user.gravatar_url(:rating => 'R').should == 'http://gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af.png?r=R'
+      @user.gravatar_url(:rating => 'R').should == 'https://secure.gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af.png?r=R'
     end
 
     it "abides to some new fancy feature" do
-      @user.gravatar_url(:extreme => true).should == 'http://gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af.png?extreme=true&r=PG'
+      @user.gravatar_url(:extreme => true).should == 'https://secure.gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af.png?extreme=true&r=PG'
     end
 
     it "makes a URL from the defaults" do
